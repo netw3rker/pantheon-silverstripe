c295671a1cdcd6dc3a1a547d48bd768e80052d50
Chris Porter <chrisporter@Chriss-MacBook-Pro.local>
2023-10-25 21:48:51 -0400

    added an environment config file

---
+++ app/_config.php
@@ -0,0 +1,16 @@
+use SilverStripe\Core\EnvironmentLoader;
+
+
+
+
+#$env = BASE_PATH . '/app/.env';
+
+define('SS_DATABASE_SERVER', $_ENV['DB_HOST']);
+define('SS_DATABASE_USERNAME', $_ENV['DB_USER']);
+define('SS_DATABASE_PASSWORD', $_ENV['DB_PASSWORD']);
+define('SS_DATABASE_NAME', $_ENV['DB_NAME']);
+define('SS_DATABASE_PORT', $_ENV['DB_PORT']);
+
+
+$loader = new EnvironmentLoader();
+$loader->loadFile($env);
